55bc9a6de2e4c25edb5f4716dd35c0a5ea0a3bce
128na <simutrans.128na@gmail.com>
2021-07-10 17:10:54 +0900

    プロジェクト表示順を変更

--- src/firebase/dataManager.js
+++ src/firebase/dataManager.js
@@ -48,7 +48,7 @@ export default {
     listen(userId, handler) {
       return users.doc(userId)
         .collection('projects')
-        .orderBy('updatedAt', 'desc')
+        .orderBy('updatedAt', 'asc')
         .onSnapshot(col => {
           handler(col.docs.map(doc => {
             return { id: doc.id, data: doc.data() };
