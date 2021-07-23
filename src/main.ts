bea6f25ce78484f8b9b842cc8c470ee1f05b4831
128na <simutrans.128na@gmail.com>
2021-07-23 17:39:51 +0900

    setup

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,11 @@ import store from './plugins/store'
 import fontawesome from "./plugins/fontawesome";
 
 import "./firebase";
+import { Quasar } from 'quasar'
+import quasarUserOptions from './plugins/quasar-user-options'
 
 const app = createApp(App)
+  .use(Quasar, quasarUserOptions)
   .use(store)
   .use(router);
 
