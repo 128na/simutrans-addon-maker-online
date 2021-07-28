dc36f8efb5a300c34399a4d477b7032cd3d943ba
128na <simutrans.128na@gmail.com>
2021-07-28 12:31:44 +0900

    ロード中表示を調整

--- src/components/LayoutLoading.vue
+++ src/components/LayoutLoading.vue
@@ -1,6 +1,23 @@
 <template>
-  <div>Loading...</div>
+  <q-inner-loading showing>
+    <q-icon color="primary" :name="iconName" size="4rem" />
+    Loading...
+  </q-inner-loading>
 </template>
 <script>
-export default {};
+const icons = [
+  "local_shipping",
+  "flight_takeoff",
+  "directions_boat",
+  "directions_bus",
+  "train",
+  "subway",
+];
+export default {
+  computed: {
+    iconName() {
+      return icons[Math.floor(Math.random() * icons.length)];
+    },
+  },
+};
 </script>
