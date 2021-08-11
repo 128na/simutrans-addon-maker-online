c1fa9f461d8e449f97dee423fa770aee7ff3424a
128na <simutrans.128na@gmail.com>
2021-08-11 18:22:04 +0900

    資料整理

---
+++ src/components/DatEditor/ObjEditor/ObjBuilding.vue
@@ -0,0 +1,36 @@
+<template>
+  <h6>基本情報</h6>
+  <div class="q-ml-md">
+    <param-name v-model="obj" />
+    <param-copyright v-model="obj" />
+  </div>
+  <h6>その他</h6>
+  <div class="q-ml-md">
+    <param-intro-retire v-model="obj" />
+  </div>
+  <h6>画像</h6>
+  <div class="q-ml-md"></div>
+</template>
+<script>
+import Params from "./ParamEditor";
+
+export default {
+  icon: "maps_home_work",
+  components: { ...Params },
+  props: ["obj", "dat", "project"],
+  emits: ["update"],
+  watch: {
+    obj() {
+      this.$emit("update");
+    },
+  },
+  computed: {
+    imageNames() {
+      return ["emptyimage"];
+    },
+    directions() {
+      return ["s", "n", "e", "w", "ne", "sw", "se", "nw"];
+    },
+  },
+};
+</script>
--- src/components/DatEditor/ObjEditor/index.ts
+++ src/components/DatEditor/ObjEditor/index.ts
@@ -1,7 +1,9 @@
 import ObjNone from './ObjNone.vue';
 import ObjVehicle from './ObjVehicle.vue';
+import ObjBuilding from './ObjBuilding.vue';
 
 export default {
   ObjNone,
   ObjVehicle,
+  ObjBuilding,
 };
